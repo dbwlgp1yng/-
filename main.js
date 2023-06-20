1281e3dcd05004ab00d89186e37b4f1b04d6a564
jihye yu <dbwlgp1yng@gmail.com>
2023-06-20 12:47:02 +0900

    JavaScript 성능개선

--- main.js
+++ main.js
@@ -3,22 +3,30 @@ const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', event => {
-    const x = event.clientX;
-    const y = event.clientY;
+window.addEventListener('load', () => {
 
-    console.log(`${x}, ${y}`);
+    const targetRect = target.getBoundingClientRect();
+    const targetRectHalfWidth = targetRect.width / 2;
+    const targetRectHalfHeight = targetRect.height / 2;
 
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-
-    horizontal.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
-
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.innerHTML = `x축: ${x}px, y축: ${y}px`;
-});
-
+    document.addEventListener('mousemove', event => {
+        const x = event.clientX;
+        const y = event.clientY;
+    
+        console.log(`${x}, ${y}`);
+    
+        target.style.transform = `
+            translate(${x - targetRectHalfWidth}px,
+            ${y - targetRectHalfHeight}px)
+        `;
+    
+        horizontal.style.transform = `translate(0,${y}px)`;
+        vertical.style.transform = `translate(${x}px,0)`;
+    
+        tag.style.transform = `translate(${x}px,${y}px)`;
+        tag.innerHTML = `x축: ${x}px, y축: ${y}px`;
+    });
+    
+})
 
 
